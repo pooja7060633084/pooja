15bf762b7fb4c12bd818722f0b01dea6fb9c1044
pooja7060633084 <31719590+pooja7060633084@users.noreply.github.com>
2017-09-07 10:04:40 +0530

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,10 @@
+nb = int(input('Enter a number DIVISIBLE BY 2'))
+while('no'):
+		if(nb%2==0):
+			print('yes')
+			break
+		else:
+			print('no')
+			nb = int(input('OOPS enter number again it seems you need to go back to school'))
+			
+			
